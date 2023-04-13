de83a0a23e940e7587a6cf581a907fbcbc438c73
ThomasCattus <sebasthy.espinoza001@mymdc.net>
2023-04-13 23:00:28 +0000

    commit mpg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#your code here!
+miles_driven = float((25))
+gallons_used = float((25))
 
-#read in the miles and read in the gallons used.
+miles_per_gallon = miles_driven / gallons_used
 
-#display the miles per gallon
+print(miles_per_gallon)
