c1d0366315d9ef4bf1046728cb0f090f595606ff
ThomasCattus <sebasthy.espinoza001@mymdc.net>
2023-04-13 23:09:12 +0000

    recent commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-miles_driven = float(input("Enter the miles driven: "))
-gallons_used = float(input("Enter the gallons used: "))
+miles_driven = float(input())
+gallons_used = float(input())
 
 miles_per_gallon = miles_driven / gallons_used
 
-print("The miles per gallon is:", miles_per_gallon)
+print(miles_per_gallon)
